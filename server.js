139e236d6111ccfc23aebf7cf77e75be7aeab027
Blancojulian <julianblanco963@hotmail.com>
2022-07-07 17:10:48 -0300

    listo heroku

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/herokuubsmi'));
+app.use(express.static('./dist/crud-angular-firebase'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/out-tsc/'})
+    res.sendFile('index.html', {root: 'dist/crud-angular-firebase/'})
 );
 
 // Start the app by listening on the default Heroku port
